6604ac48b46ce62fd7aad12682752065c2d1612a
seongjunkim <tjdwns5063@naver.com>
2023-03-01 21:24:20 +0900

    implement Reissue Logic and related view
    - implement reissue Implemented a dialog to re-confirm reissue request
    
    - Implementation of functions related to reissuance request

--- app/src/main/java/com/hane24/hoursarenotenough24/reissue/ReissueFragment.kt
+++ app/src/main/java/com/hane24/hoursarenotenough24/reissue/ReissueFragment.kt
@@ -6,18 +6,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
 import com.hane24.hoursarenotenough24.R
 import com.hane24.hoursarenotenough24.databinding.FragmentReissueBinding
 
 class ReissueFragment : Fragment() {
     private lateinit var binding: FragmentReissueBinding
+    private val viewModel: ReissueViewModel by activityViewModels()
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentReissueBinding.inflate(layoutInflater, container, false)
 
-
+        binding.reissueApplyButton.setOnClickListener { viewModel.clickReissueButton(requireActivity()) }
         return binding.root
     }
 }
--- app/src/main/java/com/hane24/hoursarenotenough24/reissue/ReissueViewModel.kt
+++ app/src/main/java/com/hane24/hoursarenotenough24/reissue/ReissueViewModel.kt
@@ -1,5 +1,8 @@
 package com.hane24.hoursarenotenough24.reissue
 
+import android.app.Activity
+import android.content.Context
+import androidx.fragment.app.FragmentActivity
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -28,14 +31,49 @@ class ReissueViewModel: ViewModel() {
         }
     }
 
+    fun clickReissueButton(activity: FragmentActivity) {
+        ReissueWarningDialog.showReissueDialog(activity.supportFragmentManager)
+    }
+    fun clickReissueOkButton() {
+        viewModelScope.launch {
+            usePostReissueApi()
+        }
+    }
+
+    fun refreshButtonOnClick() {
+        viewModelScope.launch {
+            useGetReissueStateApi()
+        }
+    }
+
     private suspend fun useGetReissueStateApi() {
-        _reissueState.value = Hane42Apis.hane42ApiService.getReissueState(accessToken)
+        try {
+            _reissueState.value = Hane42Apis.hane42ApiService.getReissueState(accessToken)
+        } catch(err: HttpException) {
+            when(err.code()) {
+                404 -> {_reissueState.value = null}
+//                503 ->
+            }
+
+        } catch (e: Exception) {
+
+        }
     }
 
     private suspend fun usePostReissueApi() {
         try {
             _reissueResult.value = Hane42Apis.hane42ApiService.postReissueRequest(accessToken)
         } catch(err: HttpException) {
+//                404 ->
+//                503 ->
+        } catch (e: Exception) {
+
+        }
+    }
+    private suspend fun usePatchReissueFinish() {
+        try {
+            _reissueResult.value = Hane42Apis.hane42ApiService.postReissueRequest(accessToken)
+        } catch (err: HttpException) {
 //                404 ->
 //                503 ->
         } catch (e: Exception) {
