ee3e5ceff7f8a63cb18040654b8cb5d5f85013ef
seongjunkim <tjdwns5063@naver.com>
2023-12-21 18:21:12 +0900

    feat: WidgetViewModel 구현
    Widget의 상태(LOADING, COMPLETE, ERROR)에 따라 적절한 뷰가 보이도록 구현
    시간 데이터는 아직 V3 적용하지 않고 V2에서 받아온 데이터로 설정되어 있음

--- app/src/main/java/com/hane24/hoursarenotenough24/widget/AppWidgetReceiver.kt
+++ app/src/main/java/com/hane24/hoursarenotenough24/widget/AppWidgetReceiver.kt
@@ -2,22 +2,22 @@ package com.hane24.hoursarenotenough24.widget
 
 import android.content.Context
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
 import androidx.glance.AndroidResourceImageProvider
-import androidx.glance.ColorFilter
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.Image
 import androidx.glance.ImageProvider
+import androidx.glance.LocalContext
 import androidx.glance.action.clickable
+import androidx.glance.appwidget.CircularProgressIndicator
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
-import androidx.glance.appwidget.appWidgetBackground
-import androidx.glance.appwidget.cornerRadius
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
@@ -34,7 +34,12 @@ import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextStyle
 import androidx.glance.unit.ColorProvider
+import com.hane24.hoursarenotenough24.App
 import com.hane24.hoursarenotenough24.R
+import com.hane24.hoursarenotenough24.network.Hane24Apis
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class AppWidgetReceiver : GlanceAppWidgetReceiver() {
     override val glanceAppWidget: GlanceAppWidget = MyAppWidget()
@@ -42,35 +47,85 @@ class AppWidgetReceiver : GlanceAppWidgetReceiver() {
 
 class MyAppWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
-        // In this method, load data needed to render the AppWidget.
-        // Use `withContext` to switch to another thread for long running
-        // operations.
+        val viewModel = WidgetViewModel(
+            Hane24Apis.hane24ApiService,
+            App.sharedPreferenceUtilss.getAccessToken()
+        )
+
+        viewModel.updateWidget(context)
+
         provideContent {
             // create your AppWidget here
-            WidgetUI()
+            WidgetUI(viewModel)
         }
     }
 }
 
 @Composable
-@Preview
-fun WidgetUI() {
+fun WidgetUI(viewModel: WidgetViewModel) {
+
+
+    val state by viewModel.state.collectAsState()
+    val monthTime by viewModel.monthAccumulationTimeInfo.collectAsState()
+    val acceptedTime by viewModel.acceptedAccumulationTime.collectAsState()
+    val context = LocalContext.current
+
+    val onClick = suspend {
+        viewModel.updateWidget(context)
+    }
+
+    when (state) {
+        WidgetState.LOADING, WidgetState.COMPLETE -> WidgetCompletePage(
+            onClick = onClick,
+            state = state,
+            monthTime = monthTime,
+            acceptedTime = acceptedTime
+        )
+
+        WidgetState.LOGIN_ERROR -> WidgetErrorPage("로그인이 되어있지 않습니다.")
+        WidgetState.UNKNOWN_ERROR -> WidgetErrorPage("에러가 발생했습니다.\n\n잠시 후 다시 시도해주세요.")
+    }
+
+}
+
+@Composable
+fun WidgetCompletePage(
+    onClick: suspend () -> Unit,
+    state: WidgetState,
+    monthTime: Pair<String, String>,
+    acceptedTime: Pair<String, String>
+) {
     Column(
         modifier = GlanceModifier.fillMaxSize()
-            .background(ImageProvider(R.drawable.app_widget_background)),
-        horizontalAlignment = Alignment.CenterHorizontally,
+            .background(ImageProvider(R.drawable.app_widget_background))
+            .padding(horizontal = 16.dp),
+        horizontalAlignment = Alignment.Start,
         verticalAlignment = Alignment.CenterVertically
     ) {
-        WidgetTitle()
+        WidgetTitle(onClick, state)
         Spacer(modifier = GlanceModifier.height(13.dp))
-        WidgetContent(title = "월 누적 시간", time = "112시간 42분", false)
+        WidgetContent(title = "월 누적 시간", time = monthTime, isChecked = false)
         Spacer(modifier = GlanceModifier.height(16.dp))
-        WidgetContent(title = "인정 시간", time = "112시간 42분", true)
+        WidgetContent(title = "인정 시간", time = acceptedTime, isChecked = true)
     }
 }
 
 @Composable
-fun WidgetTitle() {
+fun WidgetErrorPage(msg: String) {
+    Column(
+        modifier = GlanceModifier.fillMaxSize()
+            .background(ImageProvider(R.drawable.app_widget_background))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(msg)
+    }
+}
+
+
+@Composable
+fun WidgetTitle(onClick: suspend () -> Unit, state: WidgetState) {
     val titleStyle = TextStyle(
         color = ColorProvider(Color(0xff735bf2)),
         fontSize = TextUnit(12.0f, TextUnitType.Sp)
@@ -86,16 +141,24 @@ fun WidgetTitle() {
             Text(text = "24HANE", style = titleStyle)
             Text(text = "12.24 11:01 기준", style = timeStyle)
         }
-        Image(
-            modifier = GlanceModifier.clickable {}.size(16.dp),
-            provider = AndroidResourceImageProvider(R.drawable.ic_widget_refresh),
-            contentDescription = "widget_refresh"
-        )
+        if (state == WidgetState.LOADING) {
+            CircularProgressIndicator(modifier = GlanceModifier.size(16.dp))
+        } else {
+            Image(
+                modifier = GlanceModifier.clickable {
+                    CoroutineScope(Dispatchers.IO).launch {
+                        onClick()
+                    }
+                }.size(16.dp),
+                provider = AndroidResourceImageProvider(R.drawable.ic_widget_refresh),
+                contentDescription = "widget_refresh"
+            )
+        }
     }
 }
 
 @Composable
-fun WidgetContent(title: String, time: String, isChecked: Boolean) {
+fun WidgetContent(title: String, time: Pair<String, String>, isChecked: Boolean) {
     val titleStyle = TextStyle(
         fontSize = TextUnit(15.0f, TextUnitType.Sp),
         fontWeight = FontWeight.Bold
@@ -105,9 +168,10 @@ fun WidgetContent(title: String, time: String, isChecked: Boolean) {
         fontWeight = FontWeight.Medium,
         color = ColorProvider(Color(0x80000000))
     )
-    val dividerResId = if (isChecked) R.drawable.widget_divider_purple else R.drawable.widget_divider
+    val dividerResId =
+        if (isChecked) R.drawable.widget_divider_purple else R.drawable.widget_divider
 
-    Row(verticalAlignment = Alignment.CenterVertically) {
+    Row(modifier = GlanceModifier ,verticalAlignment = Alignment.CenterVertically) {
         Box {
             Spacer(
                 modifier = GlanceModifier
@@ -118,7 +182,7 @@ fun WidgetContent(title: String, time: String, isChecked: Boolean) {
         Spacer(modifier = GlanceModifier.width(6.dp))
         Column {
             Text(text = title, style = titleStyle)
-            Text(text = time, style = timeStyle)
+            Text(text = String.format("%s시간 %s분", time.first, time.second), style = timeStyle)
         }
     }
 }
---
+++ app/src/main/java/com/hane24/hoursarenotenough24/widget/WidgetViewModel.kt
@@ -0,0 +1,68 @@
+package com.hane24.hoursarenotenough24.widget
+
+import android.content.Context
+import android.util.Log
+import androidx.glance.appwidget.updateAll
+import com.hane24.hoursarenotenough24.network.AccumulationTimeInfo
+import com.hane24.hoursarenotenough24.network.Hane24Api
+import com.hane24.hoursarenotenough24.overview.ParseTimeUseCase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.stateIn
+import retrofit2.HttpException
+
+enum class WidgetState {
+    LOADING,
+    LOGIN_ERROR,
+    UNKNOWN_ERROR,
+    COMPLETE
+}
+
+class WidgetViewModel(
+    private val hane24Api: Hane24Api,
+    private val token: String?
+) {
+    private val _widget = MyAppWidget()
+
+    private val _monthAccumulationTime = MutableStateFlow(0L)
+    val monthAccumulationTimeInfo: StateFlow<Pair<String, String>> = _monthAccumulationTime.map { ParseTimeUseCase().parseAccumulationTime(it) }
+        .stateIn(CoroutineScope(Dispatchers.Default), SharingStarted.Lazily, "0" to "0")
+
+    private val _acceptedAccumulationTime = MutableStateFlow(0L)
+    val acceptedAccumulationTime: StateFlow<Pair<String, String>> = _acceptedAccumulationTime.map { ParseTimeUseCase().parseAccumulationTime(it) }
+        .stateIn(CoroutineScope(Dispatchers.Default), SharingStarted.Lazily, "0" to "0")
+
+    private val _state = MutableStateFlow<WidgetState>(WidgetState.LOADING)
+    val state = _state.asStateFlow()
+
+    suspend fun updateWidget(context: Context) {
+        _state.value = WidgetState.LOADING
+        _widget.updateAll(context)
+
+        try {
+            val accTimeInfo: AccumulationTimeInfo = hane24Api.getAccumulationTime(token)
+
+            _monthAccumulationTime.value = accTimeInfo.monthAccumulationTime
+            _acceptedAccumulationTime.value = accTimeInfo.todayAccumulationTime
+        } catch (err: Exception) {
+            if (err is HttpException && err.code() == 401) {
+                _state.value = WidgetState.LOGIN_ERROR
+            } else {
+                _state.value = WidgetState.UNKNOWN_ERROR
+            }
+            _widget.updateAll(context)
+        } finally {
+            if (_state.value == WidgetState.LOGIN_ERROR || _state.value == WidgetState.UNKNOWN_ERROR) {
+                delay(2000)
+            }
+            _state.value = WidgetState.COMPLETE
+            _widget.updateAll(context)
+        }
+    }
+}
